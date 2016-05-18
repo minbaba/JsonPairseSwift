f92c9a4f1d0453ecce482249fd725df8af7e8dcb
郑敏 <zhengmin@zhengmins-macbook-pro-2.local>
2016-05-18 10:21:53 +0800

    Initial Commit

---
+++ JsonPairseSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JsonPairseSwift
+//
+//  Created by 郑敏 on 16/5/18.
+//  Copyright © 2016年 郑敏. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JsonPairseSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JsonPairseSwift
+//
+//  Created by 郑敏 on 16/5/18.
+//  Copyright © 2016年 郑敏. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
