c0888d40d40187c185473cb00b9f6b30263dc6a5
郑敏 <zhengmin@zhengmins-MacBook-Pro-2.local>
2016-08-15 16:15:32 +0800

    first commit

--- JsonPairseSwift/AppDelegate.swift
+++ JsonPairseSwift/AppDelegate.swift
@@ -14,6 +14,67 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
+        
+        let manager = NSFileManager()
+        let enumerator = manager.enumeratorAtPath("/Users/zhengmin/Documents/miaoqu_hl/2版素材/v2/切图/会话窗口/emoji/tiny_压缩图/")
+        
+        
+        var arr = [(Int, String, String)]()
+        
+        while  let x = enumerator?.nextObject() {
+            
+            let name = x.pathComponents.last ?? ""
+            if name.hasSuffix("@3x.png") {
+                continue
+            }
+            var index = 0
+            
+            var lenth = 0
+            if Int((name as NSString).substringWithRange(NSMakeRange(0, 1))) > 0 {
+                lenth = 1
+                index = Int((name as NSString).substringWithRange(NSMakeRange(0, 1)))!
+            }
+            if Int((name as NSString).substringWithRange(NSMakeRange(0, 2))) > 0 {
+                lenth = 2
+                index = Int((name as NSString).substringWithRange(NSMakeRange(0, 2)))!
+            }
+            
+            if index > 0 {
+                let rename = (name as NSString).substringWithRange(NSMakeRange(lenth, name.characters.count - lenth)).componentsSeparatedByString("@").first ?? ""
+                arr.append((index, rename, name))
+            }
+        }
+        
+        for n in arr {
+            try! manager.moveItemAtPath("/Users/zhengmin/Documents/miaoqu_hl/2版素材/v2/切图/会话窗口/emoji/tiny_压缩图/" + n.2, toPath: "/Users/zhengmin/Documents/miaoqu_hl/2版素材/v2/切图/会话窗口/emoji/" + n.1)
+        }
+        
+        let sort = arr.sort { (fi, se) -> Bool in
+            return fi.0 < se.0
+        }
+        let arr2 = sort.map { $0.1 }
+        print(arr2)
+        
+        
+        print(sort)
+        
+        
+
+        
+//        let name = NSArray(contentsOfFile: NSBundle.mainBundle().pathForResource("Profession.plist", ofType: nil)!)
+//
+//        var arr = [String: [String]]()
+//        for dict in name as! [NSDictionary] {
+//            let name = dict["groupName"] as! String
+//            let list = dict["groupList"] as! [String]
+//            
+//            arr[name] = list
+//        }
+//        
+//        (arr as NSDictionary).writeToFile("/Users/zhengmin/Documents/hah.plist", atomically: true)
+        
+        
         // Insert code here to initialize your application
     }
 
--- JsonPairseSwift/ViewController.swift
+++ JsonPairseSwift/ViewController.swift
@@ -7,13 +7,95 @@
 //
 
 import Cocoa
+import RxSwift
+import RxCocoa
+import SwiftyJSON
 
 class ViewController: NSViewController {
+    
+    
+    @IBOutlet var inputText: NSTextView!
+    @IBOutlet var outputText: NSTextView!
+    var rx_mq_text = PublishSubject<String>()
+    let disposebag = DisposeBag()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        self.rx_mq_text
+            .distinctUntilChanged()
+            .subscribeNext {[weak self] (str) in
+                
+                
+                print(str)
+                
+                let obj = JSON(data: str.dataUsingEncoding(NSUTF8StringEncoding)!)
+                
+                var str = ""
+                if obj.type == .Dictionary {
+                
+                    for (key, value) in obj.dictionary! {
+                        
+                        str += ("dynamic var " + key + " = ")
+                        switch value.type {
+                            
+                        case .Number:
+                            
+                            str += value.stringValue
+                            
+                            break
+                        case .String:
+                            
+                            str += "\"" + value.stringValue + "\""
+                            
+                            break
+                        case .Bool:
+                            
+                            str += value.stringValue
+                            
+                            break
+                        case .Array:
+                            str += "List<>()"
+                            break
+                        case .Dictionary:
+                            
+                            break
+                        default:
+                            
+                            break
+                        }
+                        
+                        str += "\n\n"
+                    }
+                
+                }
+                
+                
+                
+                self?.outputText.string = str
+                
+            }.addDisposableTo(disposebag)
 
-        // Do any additional setup after loading the view.
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)) {
+            
+            while (true) {
+                
+                if let str = self.inputText.string {
+                    self.rx_mq_text.onNext(str)
+                }
+                
+                sleep(1)
+            }
+            
+            
+        }
+        
+    
+        
+    
     }
 
     override var representedObject: AnyObject? {
